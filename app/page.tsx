cf3077e3152293812e2a40dea7f8f30b640bd788
chandranimaheswari <chandranimaheswari13@gmail.com>
2026-05-28 17:09:19 +0530

    Next js + shadcn Installed

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center flex-col">
+      <p className="text-5xl font-bold">Rebellious Cockroches</p>
+      <p className="text-lg font-medium">Official Store</p>
+    </div>
+  );
+}
